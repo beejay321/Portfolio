10b98167e53d5318c5bbd4f51f2c61cab44591af
beejay321 <jibodu.busola@gmail.com>
2022-02-25 15:47:22 +0100

    Project initialization

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <Router>
+      <Home />
+    </Router>
+  );
+}
+
+export default App;
