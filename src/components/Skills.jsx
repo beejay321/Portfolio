312347403bfde072123c985bff7372b6c82264f9
beejay321 <jibodu.busola@gmail.com>
2022-03-03 18:16:02 +0100

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,43 +4,43 @@ import { Container, Row, Col } from "react-bootstrap";
 function Skills(props) {
   return (
     <>
-      <Container className=" py-5">
-        <Row>
+      <Container className=" sth py-5">
+        <Row className=" skills-box mx-1">
           <div>
             <h5 className=" title d-flex pb-3 justify-content-center">SKILLS</h5>
           </div>
-          <Col>
-            <div>
-              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem velit laudantium id perspiciatis, inventore doloribus repudiandae! In, molestias. A!</p>
-              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem velit laudantium id perspiciatis, inventore doloribus repudiandae! In, molestias. A!</p>
+          <Col xs={12}  lg={3} className=" d-grid skills-div justify-content-center">
+            <p className="skill-category">FRONTEND</p>
+            <div className="skill">
+              <span>HTML5</span>
+              <span>CSS</span>
+              <span>JavaScript</span>
+              <span>TypeScript</span>
+              <span>React|Redux</span>
             </div>
           </Col>
-          <Col>
-            <div className=" d-flex gap-5">
-              <div className=" d-grid skills-div justify-content-center">
-                <p>FRONTEND</p>
-                <span>HTML5</span>
-                <span>CSS</span>
-                <span>JavaScript</span>
-                <span>React|Redux</span>
-              </div>
-              <div className=" d-grid skills-div justify-content-center">
-                <p>TOOLS</p>
-                <span>Git</span>
-                <span>Heroku</span>
-                <span>OAuth</span>
-                <span>Agile</span>
-                <span>Bootstrap</span>
-              </div>
-              <div className=" d-grid skills-div justify-content-center">
-                <p>BACKEND</p>
-                <span>Node.JS</span>
-                <span>Express</span>
-                <span>MongoDB</span>
-                <span>PostgresSQL</span>
-              </div>
+          <Col xs={12}  lg={3} className=" d-grid skills-div justify-content-center ">
+            <p className="skill-category">TOOLS</p>
+            <div className="skill">
+              <span>Git</span>
+              <span>Heroku</span>
+              <span>OAuth</span>
+              <span>Agile</span>
+              <span>Bootstrap</span>
             </div>
           </Col>
+          <Col xs={12}  lg={3} className=" d-grid skills-div justify-content-center ">
+            <p className="skill-category">BACKEND</p>
+            <div className="skill">
+              <span>Node</span>
+              <span>Express</span>
+              <span>MongoDB</span>
+              <span>PostgresSQL</span>
+              <span>RESTful API</span>
+            </div>
+          </Col>
+
+         
         </Row>
       </Container>
     </>
