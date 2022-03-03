98a5d376961491deed3d40a14c5df4a0874aa1b4
beejay321 <jibodu.busola@gmail.com>
2022-03-03 18:15:58 +0100

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,125 +1,39 @@
 import React from "react";
-import { Container, Row, Col, Image } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
+import EachProject from "./EachProject";
 
 function Projects(props) {
   return (
     <Container className="project-container py-5">
       <Row>
         <div>
-          <h5 className=" title d-flex pb-3 justify-content-center">MY PROJECTS</h5>
+          <h5 className=" title pb-3 d-flex justify-content-center">PROJECTS</h5>
         </div>
       </Row>
-      <Row className=" project-div pb-5">
-        <Col xs={10} lg={5}>
-          <div className=" d-grid  pb-5">
-            <h6 className="project-title">TRAVELGO</h6>
-            <div className=" skills-used ">
-              <span>HTML5</span>
-              <span>CSS</span>
-              <span>JavaScript</span>
-              <span>React</span>
-              <span>Bootstrap</span>
-              <span>Node.js</span>
-              <span>MongoDB</span>
-              <span>GIT</span>
-            </div>
-            <p className="collaborator">
-              <span>Solo Project</span>
-            </p>
-            <p className="">
-              <span>
-                A travel log website for travellers get relevant information about prosective vacation destinations and also share experiences and information on previous vacation destinations.
-              </span>
-            </p>
-            <div className=" proj-views d-flex gap-2">
-              <span>DEMO</span>
-              <span>Source Code</span>
-            </div>
-          </div>
-        </Col>
-        <Col xs={12} lg={1}>
-          <div className=" imageBox"></div>
-        </Col>
-        <Col xs={12} lg={6}>
-          <div className=" imageBox">
-            <Image className="imageFit" fluid src="https://via.placeholder.com/600x350" alt="..." />
-          </div>
-        </Col>
-      </Row>
-      <Row className=" project-div pb-5">
-        <Col xs={10} lg={5}>
-          <div className=" d-grid  pb-5">
-            <h6 className="project-title">CLIENT CONNECT</h6>
-            <div className=" skills-used ">
-              <span>HTML5</span>
-              <span>CSS</span>
-              <span>JavaScript</span>
-              <span>React</span>
-              <span>Bootstrap</span>
-              <span>Redux</span>
-              <span>Node.js</span>
-              <span>MongoDB</span>
-              <span>GIT</span>
-            </div>
-            <p className="collaborator">
-              <span>Solo Project</span>
-            </p>
-            <p className="">
-              <span>
-                A marketplace website to connect freelancers to clients with freelance job. Freelancers have the ability to bid for projects they believe they are qualified for and the clients will
-                choose which freelancers they feel suits best{" "}
-              </span>
-            </p>
-            <div className=" proj-views d-flex gap-2">
-              <span>DEMO</span>
-              <span>Source Code</span>
-            </div>
-          </div>
-        </Col>
-        <Col xs={12} lg={1}>
-          <div className=" imageBox"></div>
-        </Col>
-        <Col xs={12} lg={6}>
-          <div className=" imageBox">
-            <Image className="imageFit" fluid src="https://via.placeholder.com/600x350" alt="..." />
-          </div>
-        </Col>
-      </Row>
-      <Row className=" project-div pb-5">
-        <Col xs={10} lg={5}>
-          <div className=" d-grid  pb-5">
-            <h6 className="project-title">WEATHERSAYS</h6>
-            <div className=" skills-used ">
-              <span>HTML5</span>
-              <span>CSS</span>
-              <span>React</span>
-              <span>TypeScript</span>
-              <span>Bootstrap</span>
-              <span>OpenWeather API</span>
-              <span>GIT</span>
-            </div>
-            <p className="collaborator">
-              <span>Solo Project</span>
-            </p>
-            <p className="">
-              <span>A website that basically tells the weather conditions for diffrent locations if searched for. The data is gotten from the Open weather </span>
-              {/* https://openweathermap.org/ */}
-            </p>
-            <div className=" proj-views d-flex gap-2">
-              <span>DEMO</span>
-              <span>Source Code</span>
-            </div>
-          </div>
-        </Col>
-        <Col xs={12} lg={1}>
-          <div className=" imageBox"></div>
-        </Col>
-        <Col xs={12} lg={6}>
-          <div className=" imageBox">
-            <Image className="imageFit" fluid src="https://via.placeholder.com/600x350" alt="..." />
-          </div>
-        </Col>
-      </Row>
+
+      <EachProject
+        projectTitle="TRAVELGO"
+        collaborators="Solo Project"
+        sourceCode="https://github.com/beejay321/Travel_go"
+        live=""
+        description="A travel log website for travellers get relevant information about prosective vacation destinations and also share experiences and information on previous vacation destinations."
+      />
+      <EachProject
+        projectTitle="CLIENT CONNECT"
+        collaborators="Solo Project"
+        sourceCode="https://github.com/beejay321/Capstone"
+        // live="https://clientconnect-fawn.vercel.app/"
+        description="A marketplace website to connect freelancers to clients with freelance job. Freelancers have the ability to bid for projects they believe they are qualified for and the clients will
+        choose which freelancers they feel suits best."
+      />
+      <EachProject
+        projectTitle="WEATHERSAYS"
+        collaborators="Solo Project"
+        sourceCode="https://github.com/beejay321/MyWeather_App"
+        live=""
+        description="A website that basically tells the weather conditions for different 
+        locations if searched for."
+      />
     </Container>
   );
 }
