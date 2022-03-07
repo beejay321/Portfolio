a85ef70af1a40431c89c88365ccd1771f4e16f4b
beejay321 <jibodu.busola@gmail.com>
2022-03-07 12:47:25 +0100

    Update Home.jsx
    Changing style of elements

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,32 +9,33 @@ import AboutMe from "./AboutMe";
 function Home(props) {
   return (
     <>
-      <Container fluid className=" heading-box  m-0 ">
-        <Row className="heading ">
-          <h1 className="name d-flex justify-content-center">OLUWABUSOLA JIBODU</h1>
-          <h5 className="title d-flex justify-content-center">FULL STACK DEVELOPER</h5>
-
-          <div className=" nav-links d-flex gap-2">
-            <a href="/#skills">
-              <span>SKILLS</span>
-            </a>
-            <a href="/#projects">
-              <span>PROJECTS</span>
-            </a>
-            <a href="/#about">
-              <span>ABOUT</span>
-            </a>
-            <a href="/#contact">
-              <span> CONTACT ME</span>
-            </a>
+      <Container fluid className=" heading  ">
+        <Row className=" heading-box   ">
+          <div>
+            <h1 className="name d-flex justify-content-center">OLUWABUSOLA JIBODU</h1>
+            <h5 className="title d-flex justify-content-center">FULL STACK DEVELOPER</h5>
+            <div className=" nav-links d-flex ">
+              <a href="/#skills">
+                <span>SKILLS</span>
+              </a>
+              <a href="/#projects">
+                <span>PROJECTS</span>
+              </a>
+              <a href="/#about">
+                <span>ABOUT</span>
+              </a>
+              <a href="/#contact">
+                <span> CONTACT ME</span>
+              </a>
+            </div>
           </div>
         </Row>
       </Container>
-      <Container>
+      {/* <Container> */}
         <Row id="skills">
           <Skills />
         </Row>
-      </Container>
+      {/* </Container> */}
       <div id="projects">
         <Projects />
       </div>{" "}
