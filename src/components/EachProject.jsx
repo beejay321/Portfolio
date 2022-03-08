9541bdab19e3038db5d438534b9271b0cd23c9f3
beejay321 <jibodu.busola@gmail.com>
2022-03-08 18:09:28 +0100

    Update EachProject.jsx
    Implementing the project data from the json file as prop

--- src/components/EachProject.jsx
+++ src/components/EachProject.jsx
@@ -1,48 +1,46 @@
 import React from "react";
-import { Row, Col, Image } from "react-bootstrap";
+import { Row, Col, Image, Carousel } from "react-bootstrap";
 
-function EachProject({ projectTitle, collaborators, description, sourceCode, live }) {
+function EachProject({ project }) {
   return (
     <>
       <Row className=" project-div pb-5">
         <Col xs={10} lg={5}>
           <Row>
             {" "}
-            <h6 className="project-title">{projectTitle}</h6>
+            <h6 className="project-title">{project.projectTitle}</h6>
           </Row>
-          <Row className=" skills-used ">
-            <Col xs={2}>HTML5</Col>
-            <Col xs={2}>CSS</Col>
-            <Col xs={2}>JavaScript</Col>
-            <Col xs={2}>React</Col>
-            <Col xs={2}>Bootstrap</Col>
-            <Col xs={2}>Node.js</Col>
-            <Col xs={2}>MongoDB</Col>
-            <Col xs={2}>GIT</Col>{" "}
-          </Row>
-          <Row className=" d-grid  pb-5">
+          <Row className=" d-grid  pb-2">
             <p>
-              <span className="collaborator">{collaborators}</span>
-              <span>{description} </span>
+              <span className="collaborator">{project.collaborators}</span>
+              <span>{project.description} </span>
             </p>
-
-            <div className=" proj-views d-flex ">
-              <a href={live} target="_blank" rel="noreferrer">
-                <span>Live Demo</span>
-              </a>
-              <a href={sourceCode} target="_blank" rel="noreferrer">
-                <span>Source Code</span>
-              </a>
-            </div>
           </Row>
+          <Row className=" skills-used  pb-3 ">
+            {project.skills.map((skill) => (
+              <Col xs={2}>{skill}</Col>
+            ))}
+          </Row>
+          <div className=" proj-views d-flex ">
+            {/* <a href={project.live} target="_blank" rel="noreferrer">
+                <span>Live Demo</span>
+              </a> */}
+            <a href={project.sourceCode} target="_blank" rel="noreferrer">
+              <span>Source Code</span>
+            </a>
+          </div>
         </Col>
         <Col xs={12} lg={1}>
           <div className=" imageBox"></div>
         </Col>
         <Col xs={12} lg={6}>
-          <div className=" imageBox">
-            <Image className="imageFit" fluid src="https://via.placeholder.com/600x350" alt="..." />
-          </div>
+          <Carousel fade>
+            {project.images.map((image) => (
+              <Carousel.Item className=" imageBox pt-3">
+                <img className="d-block w-100 imageFit" src={image} alt="" />
+              </Carousel.Item>
+            ))}
+          </Carousel>
         </Col>
       </Row>
     </>
