72380430e13d04302b1c54ed221ae974a6f7ba91
beejay321 <jibodu.busola@gmail.com>
2022-03-08 18:08:54 +0100

    Update Projects.jsx
    Implementing the data from the json file and passing it as prop to the Each project component.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container, Row } from "react-bootstrap";
 import EachProject from "./EachProject";
+import projects from "../file/projects.json";
 
 function Projects(props) {
   return (
@@ -11,29 +12,9 @@ function Projects(props) {
         </div>
       </Row>
 
-      <EachProject
-        projectTitle="TRAVELGO"
-        collaborators="Solo Project"
-        sourceCode="https://github.com/beejay321/Travel_go"
-        live=""
-        description="A travel log website for travellers get relevant information about prosective vacation destinations and also share experiences and information on previous vacation destinations."
-      />
-      <EachProject
-        projectTitle="CLIENT CONNECT"
-        collaborators="Solo Project"
-        sourceCode="https://github.com/beejay321/Capstone"
-        // live="https://clientconnect-fawn.vercel.app/"
-        description="A marketplace website to connect freelancers to clients with freelance job. Freelancers have the ability to bid for projects they believe they are qualified for and the clients will
-        choose which freelancers they feel suits best."
-      />
-      <EachProject
-        projectTitle="WEATHERSAYS"
-        collaborators="Solo Project"
-        sourceCode="https://github.com/beejay321/MyWeather_App"
-        live=""
-        description="A website that basically tells the weather conditions for different 
-        locations if searched for."
-      />
+      <EachProject project={projects[0]} />
+      <EachProject project={projects[1]} />
+      <EachProject project={projects[2]} />
     </Container>
   );
 }
