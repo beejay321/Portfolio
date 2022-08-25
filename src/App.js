702df6f1d93441e27600c41ff2f838f11e25c9ac
beejay321 <jibodu.busola@gmail.com>
2022-08-25 02:20:58 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import Home from "./components/Home";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <Router>
+      <NavBar />
       <Home />
     </Router>
   );
