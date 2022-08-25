b417ba5db4f155cabb28e7de808c998120cecac0
beejay321 <jibodu.busola@gmail.com>
2022-08-25 02:21:17 +0200

    Update AboutMe.jsx

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,26 +1,35 @@
 import React from "react";
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Col, Image } from "react-bootstrap";
 
 function AboutMe(props) {
   return (
     <Container className=" aboutSection py-5">
       <Row className="  py-5">
-        <div>
-          <h5 className=" title d-flex py-3 justify-content-center">ABOUT ME</h5>
-        </div>
-        {/* </Row>
-      <Row> */}
-        <div className="aboutText d-flex gap-2">
-          <span>
-            An architect turned web developer. In 2021, I took a leap and trained in Full stack web development at Strive School. I quickly absorbed new concepts, while bringing ambitious ideas and a
-            positive attitude to every challenge. When I'm not coding, I'm usually travelling, trying new recipes, or watching a movie.
-          </span>
-        </div>
-        <div className=" resume d-flex gap-2">
-          <a href="https://drive.google.com/file/d/1ubctSFFgJf-1jXhqxmuhNvuH5I-9JBou/view?usp=sharing" target="_blank" rel="noreferrer">
-            <span>RESUME</span>
-          </a>
-        </div>
+        <Col xs={{ offset: 2, span: 3 }}>
+          <div className="myPic-div">
+            {/* <Image className="myPic" src="https://via.placeholder.com/300x450" alt="" roundedCircle /> */}
+            <Image className="myPic" src="https://res.cloudinary.com/dvyids286/image/upload/v1661385850/portfolio/fzbl9xqyp8qn1h8dmq6l.jpg" alt="" roundedCircle />
+          </div>{" "}
+        </Col>
+        <Col className="aboutText" xs={6}>
+          <div>
+            <div>
+              <h5 className=" aboutText-title pb-3 ">Hi I'm Oluwabusola Jibodu </h5>
+            </div>
+
+            <div className="aboutText d-flex gap-2">
+              <span>
+                An architect turned web developer. In 2021, I took a leap and trained in Full stack web development at Strive School. I quickly absorbed new concepts, while bringing ambitious ideas
+                and a positive attitude to every challenge. When I'm not coding, I'm usually travelling, trying new recipes, or watching a movie.
+              </span>
+            </div>
+            <div className=" resume d-flex gap-2">
+              <a href="https://docs.google.com/document/d/1eHAUsdxh8J53QIvTRZA6ZmZHbiwiTMUaqt-bCDyoVdI/edit?usp=sharing" target="_blank" rel="noreferrer">
+                <span>RESUME</span>
+              </a>
+            </div>
+          </div>
+        </Col>
       </Row>
     </Container>
   );
